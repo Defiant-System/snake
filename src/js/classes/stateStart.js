66a7386622ab8a0a8e32d52fd76485d93a73e1e6
Hakan Bilgin <hbi99@hotmail.com>
2023-07-13 23:20:55 +0200

    persistent best score

--- src/js/classes/stateStart.js
+++ src/js/classes/stateStart.js
@@ -6,9 +6,14 @@ class StateStart {
 
 	init() {
 		let Content = snake.content,
-			Play = game.states.play;
+			Play = game.states.play,
+			cname = "show-start";
 
-		Content.removeClass("show-pause show-fireworks").addClass("show-start");
+		if (bestScore > 0) {
+			cname += " show-score";
+			Content.find(".best-score h2 span").html(bestScore);
+		}
+		Content.removeClass("show-pause show-fireworks").addClass(cname);
 
 		if (Play.score > bestScore) {
 			Content.find(".best-score h2 span").html(Play.score);
