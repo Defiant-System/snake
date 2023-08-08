8124a54a78aeca8b39b7c207b6e738865786646e
Hakan Bilgin <hbi99@hotmail.com>
2023-08-08 23:27:10 +0200

    version bump

--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -1,6 +1,7 @@
 
 let Test = {
 	init() {
+		// setTimeout(() => window.dialog.alert({message: "test", onOk: () => console.log(123)}), 300);
 		// setTimeout(() => snake.dispatch({ type: "window.keystroke", char: "p" }), 500);
 
 		// setTimeout(() => game.setState("over"), 3000);
