c60e893592e7d3f52e4c9ad32a05d8cd012392d4
Hakan Bilgin <hbi99@hotmail.com>
2023-10-01 13:05:25 +0200

    buggfix

--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -1,10 +1,11 @@
 
 let Test = {
 	init() {
-		// setTimeout(() => window.dialog.alert({message: "test", onOk: () => console.log(123)}), 300);
+		// setTimeout(() => window.dialog.alert({ message: "test", onOk: () => console.log(123) }), 300);
 		// setTimeout(() => snake.dispatch({ type: "window.keystroke", char: "p" }), 500);
 
 		// setTimeout(() => game.setState("over"), 3000);
+		// setTimeout(() => game.setState("new"), 300);
 	}
 };
 
